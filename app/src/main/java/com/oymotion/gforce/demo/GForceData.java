dc4a094534cb037de6ee7c31957218e12c7a8286
Zhou Yu <yuzhou@oymotion.com>
2016-10-12 16:45:25 +0800

    Fixed the issue of no gesture.

--- app/src/main/java/com/oymotion/gforce/demo/GForceData.java
+++ app/src/main/java/com/oymotion/gforce/demo/GForceData.java
@@ -1,5 +1,8 @@
 package com.oymotion.gforce.demo;
 
+import android.util.Log;
+import android.util.SparseArray;
+
 import java.nio.ByteBuffer;
 import java.nio.ByteOrder;
 import java.util.Arrays;
@@ -9,6 +12,8 @@ import java.util.Arrays;
  */
 
 public class GForceData {
+    private final static String TAG = GForceData.class.getSimpleName();
+
     public static final int QUATERNION_FLOAT      = 0x2;
     public static final int GESTURE               = 0xF;
 
@@ -19,16 +24,21 @@ public class GForceData {
     public static final int GESTURE_WAVE_BACKWARD_PALM     = 0x4;
     public static final int GESTURE_TUCK_FINGERS           = 0x5;
     public static final int GESTURE_SHOOT                  = 0x6;
+    public static final int GESTURE_MAX                    = GESTURE_SHOOT;
+    public static final int GESTURE_UNKNOWN                = 0xFF;
 
-    private static final String[] mGestureNames = {
-            "GESTURE_RELAX",
-            "GESTURE_GIST",
-            "GESTURE_SPREAD_FINGERS",
-            "GESTURE_WAVE_TOWARD_PALM",
-            "GESTURE_WAVE_BACKWARD_PALM",
-            "GESTURE_TUCK_FINGERS",
-            "GESTURE_SHOOT"
-    };
+    private static final SparseArray<String> mGestureNames = new SparseArray<>();
+
+    static {
+        mGestureNames.put(GESTURE_RELAX, "GESTURE_RELAX");
+        mGestureNames.put(GESTURE_GIST, "GESTURE_GIST");
+        mGestureNames.put(GESTURE_SPREAD_FINGERS, "GESTURE_SPREAD_FINGERS");
+        mGestureNames.put(GESTURE_WAVE_TOWARD_PALM, "GESTURE_WAVE_TOWARD_PALM");
+        mGestureNames.put(GESTURE_WAVE_BACKWARD_PALM,  "GESTURE_WAVE_BACKWARD_PALM");
+        mGestureNames.put(GESTURE_TUCK_FINGERS, "GESTURE_TUCK_FINGERS");
+        mGestureNames.put(GESTURE_SHOOT,  "GESTURE_SHOOT");
+        mGestureNames.put(GESTURE_UNKNOWN,  "GESTURE_UNKNOWN");
+    }
 
     private int mType;
     private float[] mQuaternionFloat;
@@ -67,7 +77,14 @@ public class GForceData {
         }
         else if (type == GESTURE) {
             if (data[1] != 1) {
-                int gesture = (int)data[2];
+                return null;
+            }
+            int gesture = (int)data[2];
+            if (gesture > GESTURE_MAX && gesture != GESTURE_UNKNOWN) {
+                Log.e(TAG, String.format("Illegal gesture value: %d", gesture));
+                return null;
+            }
+            else {
                 return new GForceData(type, gesture);
             }
         }
@@ -83,11 +100,11 @@ public class GForceData {
     }
 
     public String getGestureName(){
-        return mGestureNames[mGesture];
+        return mGestureNames.get(mGesture);
     }
 
     public static String getGestureName(int gesture){
-        return mGestureNames[gesture];
+        return mGestureNames.get(gesture);
     }
 
 }
